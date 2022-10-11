7b45bc4b65fcc8f863ac4d68cd61af10f1dfc57a
1M@gin320 <jessicasilvasousa.eng@gmail.com>
2022-10-11 19:31:43 -0400

    update commit

---
+++ aula33/assets/js/main.js
@@ -0,0 +1,10 @@
+/*
+Data em javascript
+
+*/
+
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+h1.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short'});
+
+

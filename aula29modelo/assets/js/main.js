acbd3b9f5d6a1f57a701c0fe4760ef5bbacdb754
Jéssica Silva <jessicasilvadesousa.eng@gmail.com>
2022-10-21 18:28:58 -0400

    primeiro commit

--- aula29modelo/assets/js/main.js
+++ aula29modelo/assets/js/main.js
@@ -1,1 +1,40 @@
-alert(1);
+// função que formata a hora
+function formataSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+
+// selecionando os elementos
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaContagem() {
+    timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = formataSegundos(segundos);
+    }, 1000);
+}
+
+// captura os eventos do click
+iniciar.addEventListener('click', function(e){
+    clearInterval(timer);
+    iniciaContagem();
+});
+
+pausar.addEventListener('click', function(e) {
+    clearInterval(timer);
+});
+
+zerar.addEventListener('click', function(e) {
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});

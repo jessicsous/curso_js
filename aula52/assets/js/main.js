831a94b4cce545a600d36e588b012c15de3caa84
Jéssica Silva <jessicasilvadesousa.eng@gmail.com>
2022-10-22 22:47:02 -0400

    primeiro commit

--- aula52/assets/js/main.js
+++ aula52/assets/js/main.js
@@ -19,12 +19,23 @@ function limpaInput() {
     inputTarefa.focus();
 }
 
+function criaBotaoApagar() {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'apagar esta tarefa');
+    li.appendChild(botaoApagar);
+}
+
 
 function criaTarefa(tInput) {
     const li = criaLi();
     li.innerText = tInput;
     tarefas.appendChild(li);
     limpaInput();
+    criaBotaoApagar(li);
+    salvaTarefa();
 }
 
 
@@ -32,3 +43,22 @@ btnTarefa.addEventListener('click', function() {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
+});
+
+function salvaTarefa() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+    
+    for (let tarefa of liTarefas) {
+        const tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('apagar');
+        listaDeTarefas.push(tarefaTexto);
+    }
+}

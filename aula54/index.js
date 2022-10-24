aac6ca4279a242f84ab3c5bed48de3faf91a604c
Jéssica Silva <jessicasilvadesousa.eng@gmail.com>
2022-10-23 21:58:19 -0400

    primeiro commit

--- aula54/index.js
+++ aula54/index.js
@@ -17,3 +17,13 @@ function argumento() {
     console.log(arguments[1]);
 }
 argumento(1,2,3,4,5,6,7,8);
+
+// arguments com for
+function fun() {
+    let total = 0;
+    for (let argumento of arguments) {
+        total += argumento;
+    }
+    console.log(total);
+}
+fun(1,2,3,4,5,6,7);

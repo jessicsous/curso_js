4836fcb42c1bf224240c9208ece36d4b61757506
Jéssica Silva <jessicasilvadesousa.eng@gmail.com>
2022-10-24 17:27:04 -0400

    primeiro commit

--- aula55/index.js
+++ aula55/index.js
@@ -15,3 +15,19 @@ function soma2(a, b) {
 }
 
 soma2(7, 8)
+
+// função que retorna um objeto
+function criaPessoa(nome, sobrenome) {
+    return { nome, sobrenome};
+}
+const pessoa = criaPessoa('jessica', 'Silva');
+// a linha acima é exatamente igual a linha abaixo, a diferença é que a função faz o trabalho na linha acima
+// na linha abaixo, as pessoas são criadas de forma manual
+const pessoa2 = {
+    nome: 'jessica',
+    sobrenome: 'silva'
+}; // a expressão tem sinal de igual '=' por isso usar ponto e vírgula
+
+console.log(pessoa);
+console.log(pessoa2);
+

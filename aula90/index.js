692800186f3748030bff70ddbd94f8b8548c17a2
Jéssica Silva <jessicasilvadesousa.eng@gmail.com>
2023-01-25 02:10:59 -0400

    update

---
+++ aula90/index.js
@@ -0,0 +1,46 @@
+// promises
+
+// função para executar de forma aleatória
+function aleatorio(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);}
+
+// com promise
+function espera(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+    });
+}
+
+espera('frase 1', rand(1, 3))
+  .then(resposta => {
+    console.log(resposta);
+    return espera('frase 2', rand(1, 3));
+  })
+  .then(resposta => {
+    console.log(resposta);
+    return espera('frase 3', rand(1, 3));
+  })
+  .then(resposta => {
+    console.log(resposta);
+  }).then(() => {
+    console.log('exibe na tela');
+  })
+  .catch();
+
+
+
+
+// com função de callback    
+// function espera(msg, tempo) {
+//     setTimeout(() => {
+//       console.log(msg);
+//     }, tempo);
+// }
+
+espera('frase 1', aleatorio(1, 5));
+espera('frase 2', aleatorio(2, 4));
+espera('frase 3', aleatorio(4, 6));
